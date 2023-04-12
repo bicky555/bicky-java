9f37a1bd603d1cfe74c632fb58d4f68874d6a92a
Bicky Jaiswal <bickyjaiswal90@gmail.com>
2023-04-12 14:03:41 +0530

    bick

---
+++ src/DataSizeRange.java
@@ -0,0 +1,7 @@
+public class DataSizeRange {
+    public static void main(String[] arg){
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.BYTES);
+    }
+}
