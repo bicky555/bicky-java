110ac85795d11ff5c4f3957ce36311f32f5f2172
Bicky Jaiswal <bickyjaiswal90@gmail.com>
2023-04-12 14:55:40 +0530

    created package

--- src/DataSizeRange.java
+++
@@ -1,7 +0,0 @@
-public class DataSizeRange {
-    public static void main(String[] arg){
-        System.out.println(Integer.MAX_VALUE);
-        System.out.println(Integer.MIN_VALUE);
-        System.out.println(Integer.BYTES);
-    }
-}
--- src/DataTypeDetails.java
+++
@@ -1,6 +0,0 @@
-public class DataTypeDetails {
-    public static void main(String[] arg){
-        int x = -5;
-        System.out.println(Integer.toBinaryString(x));
-    }
-}
